b5d5ed66ca950dd7dc06dac151105d9a4c073994
liqing1993 <34296124+liqing1993@users.noreply.github.com>
2019-11-04 18:56:48 +0800

    Update Feature.h
    this file include all the matching features.

--- Feature.h
+++ Feature.h
@@ -1,13 +1,11 @@
 class FeatureDescriptor
 {
-
 //image attribution
 string videosName;
 int frame;//the image in videos number
 Bbox bbox;//the person's bounding box;
 Time  time;//capture time
 Location loc;//capture location
-
 
 //person  feature
 Dress dress;
@@ -17,18 +15,40 @@ int *deepFeature;
 }
 
 
-class Bbox
+struct Bbox
 {
-  
+  public:
+  Bbox(int x1, int y1, int x2, int y2):left_up(x1, y1), right_down(x2, y2) { }
+  Bbox(cv::point2d a, cv::point2d b):left_up(a), right_down(b) { }
+  public:
+  cv::point2d left_up;
+  cv::point2d right_down;
 }
 
 class Image
 {
-
+  
 }
 
-class Dress
+struct Dress
 {
+  bool hat=0;
+  bool eyeglasses=0;
+}
 
+struct Location
+{
+  string province;
+  string urban;
+  string Area;
+}
 
+struct Time
+{
+  string year;
+  string month;
+  string day;
+  string hour;
+  string mint;
+  string seconds;
 }
